0cef6693391d44c51650b22964ff31b66021e9e5
TulasiJagan49 <jaganmeruga777@gmail.com>
2024-09-28 10:50:54 +0530

    feat(routes): exploring the routing patterns with different http methods

--- src/app.js
+++ src/app.js
@@ -2,17 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello from the server!!!");
+// We can use strings, string patterns, regex patterns
+// to match the route of a method.
+// : - Used to define route parameters.
+// req.params - to get the params as a object
+// req.query - to get the query parameters as a object
+app.get("/user", (req, res) => {
+    res.send([{ firstName: "John", lastName: "Wick" }]);
 });
 
-app.use("/hello", (req, res) => {
-    res.send("Hello from another route!!!");
-});
+app.post("/user", (req, res) => {
+    res.send("Successfully added user data to database")
+})
 
+// Use method will be called whenever the route is matched. 
 app.use("/test", (req, res) => {
     res.send("Hello from test route!!!");
 });
+
+app.use("/", (req, res) => {
+    res.send("Hello from the server!!!")
+})
 
 app.listen(3000, () => {
     console.log('Server is successfully listening on port 3000...')
